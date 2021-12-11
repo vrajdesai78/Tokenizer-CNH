84539869d53fd0182b1dcb82070b78fb119d4715
vrajdesai78 <vrajdesai78@gmail.com>
2021-12-11 22:20:37 +0530

    Authenication Added

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask
-from flask import render_template
-from firebase_admin import db, credentials, initialize_app, firestore
+from flask import render_template, request, redirect, url_for
+import pyrebase
 
 app = Flask(__name__)
 
-cred = credentials.Certificate('config.json')
-initialize_app(cred)
-db = firestore.client()
+firebaseConfig = {
+  "apiKey": "AIzaSyC4eW5vu9OGGOWhcrHpL85luyukeJBXD_A",
+  "authDomain": "starry-acre-243615.firebaseapp.com",
+  "databaseURL": "https://starry-acre-243615.firebaseio.com",
+  "projectId": "starry-acre-243615",
+  "storageBucket": "starry-acre-243615.appspot.com",
+  "messagingSenderId": "505051276034",
+  "appId": "1:505051276034:web:351104ecc519de344b981b"
+}
+
+firebase=pyrebase.initialize_app(firebaseConfig)
+auth=firebase.auth()
 
 @app.route('/')
 def home():
@@ -18,10 +27,56 @@ def places():
     place_ref = db.collection(u'Places').stream()
     for place in place_ref:
         place_details.append(place.to_dict())
-        # for key, value in place.to_dict().items():
-        #     place_details[key] = value
 
     return render_template('places.html', places=place_details)
 
+@app.route('/booking')
+def booking():
+    return render_template('booking.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def userlogin():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = auth.sign_in_with_email_and_password(email, password)
+        print("User logged in successfully")
+        return redirect(url_for('home'))
+    return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def userRegister():
+    if request.method == 'POST':
+        email = request.form['email']
+        name = request.form['name']
+        password = request.form['password']
+        confirm_password = request.form['confirm_password']
+        if password == confirm_password:
+            auth.create_user_with_email_and_password(email, password)
+            print("User created successfully")
+            return redirect(url_for('home'))
+        return render_template('signup.html')
+    return render_template('signup.html')
+
+@app.route('/b_login', methods=['GET', 'POST'])
+def b_login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = auth.sign_in_with_email_and_password(email, password)
+        print("User logged in successfully")
+        return redirect(url_for('home'))
+    return render_template('b_login.html')
+
+@app.route('/b_signup', methods=['GET', 'POST'])
+def b_signup():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        firm_category= request.form['firm']
+        
+
+    return render_template('b_signup.html')
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ firebase_test.py
@@ -0,0 +1,17 @@
+import pyrebase
+
+firebaseConfig = {
+  "apiKey": "AIzaSyC4eW5vu9OGGOWhcrHpL85luyukeJBXD_A",
+  "authDomain": "starry-acre-243615.firebaseapp.com",
+  "databaseURL": "https://starry-acre-243615.firebaseio.com",
+  "projectId": "starry-acre-243615",
+  "storageBucket": "starry-acre-243615.appspot.com",
+  "messagingSenderId": "505051276034",
+  "appId": "1:505051276034:web:351104ecc519de344b981b"
+}
+
+firebase=pyrebase.initialize_app(firebaseConfig)
+auth=firebase.auth()
+
+user = auth.sign_in_with_email_and_password("helloworld1@gmail.com", "123456")
+print(auth.get_account_info(user['idToken']).get('users')[0].get('email'))
